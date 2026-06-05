aeb26f28017d69ca039394609a3296f97c7128a0
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-06-05 20:50:28 +0800

    Case senstive linux filename

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -22,7 +22,7 @@ class LoginPage extends StatelessWidget {
           child: Column(
             children: [
               // Logo
-              Image.asset('assets/img/Logo.png', height: 200),
+              Image.asset('assets/img/logo.png', height: 200),
               const SizedBox(height: 40),
 
               // Judul
