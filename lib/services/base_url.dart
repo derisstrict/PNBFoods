c6391a51a52cca7793086a998a10aa3a5cb2fb11
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-06-13 16:41:00 +0800

    Fixed wrong url in emulator

--- lib/services/base_url.dart
+++ lib/services/base_url.dart
@@ -1,6 +1,6 @@
 class GetUrl {
   static final String localhost = 'http://localhost:8000/api/';
-  static final String emulator = 'http://10.0.0.2:8000/api/';
+  static final String emulator = 'http://10.0.2.2:8000/api/';
 }
 
 class BaseUrl {
