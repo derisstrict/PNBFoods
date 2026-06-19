a72baf6230c7e0983dc0493811be0bdfabc673ec
Moses Christian Adi <161395601+Melxieee@users.noreply.github.com>
2026-06-19 10:18:30 +0700

    ganti bagian dashbord sama kantin

--- lib/models/kantin.dart
+++ lib/models/kantin.dart
@@ -17,12 +17,16 @@ class Kantin {
 
   factory Kantin.fromJson(Map<String, dynamic> json) {
     return Kantin(
-      id: json['id'] as int,
+      id: json['id'] is int 
+          ? json['id'] as int 
+          : int.tryParse(json['id']?.toString() ?? '') ?? 0,
       namaKantin: json['nama_kantin'] as String,
       fotoKantin: json['foto_kantin'] as String?,
       fotoUrl: json['foto_url'] as String?,
       kategori: json['kategori'] as String,
-      idPenjual: json['penjual_id'] as int
+      idPenjual: json['penjual_id'] is int 
+          ? json['penjual_id'] as int 
+          : int.tryParse(json['penjual_id']?.toString() ?? '') ?? 0,
     );
   }
 }
