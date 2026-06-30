db15c4d15fb590784f32dd5ee5cce83c4d28060c
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-06-30 20:53:13 +0800

    Login appearance has been adjusted
    Now user roles can be selected in this page and added login as guest button

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -4,6 +4,7 @@ import 'package:pnbfoods/auth/autentikasi.dart';
 import 'package:pnbfoods/common/warna.dart';
 import 'package:pnbfoods/common/login_form.dart'; 
 import 'package:pnbfoods/auth/forgot_password_page.dart';
+import 'package:pnbfoods/homepage/home.dart';
 import 'package:pnbfoods/pembeli/list_produk/list_produk.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:dio/dio.dart';
@@ -37,11 +38,11 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Warna.warnaBackground,
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(
-            horizontal: 30,
+            horizontal: 20,
             vertical: 40,
           ), // ← atur angka ini
           child: Column(
@@ -50,56 +51,137 @@ class _LoginPageState extends State<LoginPage> {
               Image.asset('assets/img/logo.png', height: 200),
               const SizedBox(height: 40),
 
-              // Judul
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Masuk",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    Wrap(
+                      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      runAlignment: WrapAlignment.spaceEvenly,
+                      alignment: WrapAlignment.center,
+                      runSpacing: 10,
+                      spacing: 10,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage(role: 'pelanggan')));
+                          },
+                          style: TextButton.styleFrom(
+                            backgroundColor: widget.role == 'pelanggan' ? Warna.warnaAccent : Colors.white,
+                            foregroundColor: widget.role == 'pelanggan' ? Colors.white : Colors.black
+                          ), 
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.person),
+                              Text("Login pengguna", 
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400
+                                ),
+                              ),
+                            ],
+                          )
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage(role: 'penjual')));
+                          },
+                          style: TextButton.styleFrom(
+                            backgroundColor: widget.role == 'penjual' ? Warna.warnaAccent : Colors.white,
+                            foregroundColor: widget.role == 'penjual' ? Colors.white : Colors.black
+                          ), 
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.store_mall_directory),
+                              Text("Login penjual", 
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400
+                                ),
+                              ),
+                            ],
+                          ) 
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Masuk",
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        widget.role == 'pelanggan'
+                          ? "Silahkan masuk dengan NIM"
+                          : "Silahkan masuk dengan Email",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    if (_isLoading)
+                    const Center(child: CircularProgressIndicator())
+                    else if (widget.role == 'pelanggan')
+                      LoginForm(
+                        controller: _nimController,
+                        passController: _passwordController,
+                        labelForm: "NIM",
+                        onLogin: _login,
+                        onLupaPassword: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ForgotPasswordPage(role: 'pelanggan'),
+                            ),
+                          );
+                        },
+                      )
+                    else 
+                      LoginForm(
+                        controller: _emailController,
+                        passController: _passwordController,
+                        labelForm: "Email",
+                        onLogin: _login,
+                        onLupaPassword: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ForgotPasswordPage(role: 'penjual'),
+                            ),
+                          );
+                        },
+                      ),
+                  ],
                 ),
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  widget.role == 'pelanggan'
-                    ? "Silahkan masuk dengan NIM"
-                    : "Silahkan masuk dengan Email",
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ),
-              const SizedBox(height: 20),
-              if (_isLoading)
-              const Center(child: CircularProgressIndicator())
-              else if (widget.role == 'pelanggan')
-                LoginForm(
-                  controller: _nimController,
-                  passController: _passwordController,
-                  labelForm: "NIM",
-                  onLogin: _login,
-                  onLupaPassword: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ForgotPasswordPage(role: 'pelanggan'),
+              if (widget.role == 'pelanggan')
+              Column(
+                children: [
+                  SizedBox(height: 10,),
+                  TextButton(
+                    onPressed: () async {
+                      final prefs = await SharedPreferences.getInstance();
+                      await prefs.setString('role', 'pelanggan');
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePengguna()));
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Warna.warnaAccent
+                    ),
+                    child: Text("Login sebagai tamu",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400
                       ),
-                    );
-                  },
-                )
-              else 
-                LoginForm(
-                  controller: _emailController,
-                  passController: _passwordController,
-                  labelForm: "Email",
-                  onLogin: _login,
-                  onLupaPassword: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ForgotPasswordPage(role: 'penjual'),
-                      ),
-                    );
-                  },
-                ),
+                    )
+                  )
+                ],
+              )
             ],
           ),
         ),
