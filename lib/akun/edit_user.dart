95d3c2ec2cea5e998c747cd232afc8d0a2881756
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-07-06 13:23:52 +0800

    Prevent keyboard from triggering overflow

--- lib/akun/edit_user.dart
+++ lib/akun/edit_user.dart
@@ -59,106 +59,109 @@ class _EditProfileState extends State<EditProfile> {
       appBar: TopBar(title: "Edit Profil", icon: Icons.person_outline,),
       backgroundColor: Warna.warnaBackground,
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 35),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Container(
-                      width: 139,
-                      height: 139,
-                      color: Color(0xFFD8BED0),
-                      child: _fotoProfil != null
-                        ? Image.file(
-                            _fotoProfil!,
-                            fit: BoxFit.cover,
-                          )
-                        : (!_fotoProfileDihapus && (widget.pelanggan?.fotoUrl != null || widget.penjual?.fotoUrl != null))
-                            ? Image.network(
-                                widget.pelanggan?.fotoUrl ?? widget.penjual?.fotoUrl ?? '',
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return const Icon(
-                                    Icons.person,
-                                    size: 80,
-                                    color: Colors.white,
-                                  );
-                                },
-                              )
-                            : const Icon(
-                                Icons.person,
-                                size: 80,
-                                color: Colors.white,
-                              ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 35),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Container(
+                        width: 139,
+                        height: 139,
+                        color: Color(0xFFD8BED0),
+                        child: _fotoProfil != null
+                          ? Image.file(
+                              _fotoProfil!,
+                              fit: BoxFit.cover,
+                            )
+                          : (!_fotoProfileDihapus && (widget.pelanggan?.fotoUrl != null || widget.penjual?.fotoUrl != null))
+                              ? Image.network(
+                                  widget.pelanggan?.fotoUrl ?? widget.penjual?.fotoUrl ?? '',
+                                  fit: BoxFit.cover,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return const Icon(
+                                      Icons.person,
+                                      size: 80,
+                                      color: Colors.white,
+                                    );
+                                  },
+                                )
+                              : const Icon(
+                                  Icons.person,
+                                  size: 80,
+                                  color: Colors.white,
+                                ),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TombolNavigasi(function: () => _pickImage(), backgroundColor: Colors.white, foregroundColor: Colors.black, text: "Ganti"),
-                      SizedBox(width: 20),
-                      TombolNavigasi(function: () => _confirmDeleteProfile(context), backgroundColor: Colors.white, foregroundColor: Colors.red, text: "Hapus"),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Text("Ukuran gambar maksimal 5MB.",
-                    style: TextStyle(fontSize: 10, color: Warna.warnaTextGray, fontWeight: FontWeight.w400),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TombolNavigasi(function: () => _pickImage(), backgroundColor: Colors.white, foregroundColor: Colors.black, text: "Ganti"),
+                        SizedBox(width: 20),
+                        TombolNavigasi(function: () => _confirmDeleteProfile(context), backgroundColor: Colors.white, foregroundColor: Colors.red, text: "Hapus"),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Text("Ukuran gambar maksimal 5MB.",
+                      style: TextStyle(fontSize: 10, color: Warna.warnaTextGray, fontWeight: FontWeight.w400),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.all(20),
-              child: Text("Identitas Pengguna", 
-                style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black, fontSize: 16.0),
+              SizedBox(height: 8),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(20),
+                child: Text("Identitas Pengguna", 
+                  style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black, fontSize: 16.0),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  CustomTextFieldProfile(
-                    controller: widget.pelanggan != null ? _namaPelanggan : _namaPenjual,
-                    hintText: widget.pelanggan != null ? "Nama Lengkap" : "Nama Penjual",
-                    icon: Icons.person_outline,
-                  ),
-                  SizedBox(height: 10),
-                  CustomTextFieldProfile(
-                    controller: widget.pelanggan != null ? _nim : _emailPenjual,
-                    hintText: widget.pelanggan != null ? "NIM" : "Email",
-                    icon: widget.pelanggan != null 
-                      ? Icons.phone_android_outlined 
-                      : Icons.email_outlined,
-                  ),
-                ],
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    CustomTextFieldProfile(
+                      controller: widget.pelanggan != null ? _namaPelanggan : _namaPenjual,
+                      hintText: widget.pelanggan != null ? "Nama Lengkap" : "Nama Penjual",
+                      icon: Icons.person_outline,
+                    ),
+                    SizedBox(height: 10),
+                    CustomTextFieldProfile(
+                      controller: widget.pelanggan != null ? _nim : _emailPenjual,
+                      hintText: widget.pelanggan != null ? "NIM" : "Email",
+                      icon: widget.pelanggan != null 
+                        ? Icons.phone_android_outlined 
+                        : Icons.email_outlined,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  TombolNavigasi(function: () {Navigator.pop(context);}, backgroundColor: Colors.white, foregroundColor: Colors.black, text: "Kembali"),
-                  Spacer(),
-                  TombolNavigasi(
-                    function: () => _showSaveConfirmationDialog(context),
-                    backgroundColor: Warna.warnaAccent, 
-                    foregroundColor: Colors.white,
-                    icon: Icons.check, 
-                    text: "Simpan"),
-                ],
+              SizedBox(height: 30),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    TombolNavigasi(function: () {Navigator.pop(context);}, backgroundColor: Colors.white, foregroundColor: Colors.black, text: "Kembali"),
+                    Spacer(),
+                    TombolNavigasi(
+                      function: () => _showSaveConfirmationDialog(context),
+                      backgroundColor: Warna.warnaAccent, 
+                      foregroundColor: Colors.white,
+                      icon: Icons.check, 
+                      text: "Simpan"),
+                  ],
+                ),
               ),
-            ),
-          ]
-        ),
+            ]
+          ),
+        ) 
+        
       ),
     );
   }
