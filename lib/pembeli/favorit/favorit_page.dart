5847beac01b350eb6c1363b58d17840eb2593bd5
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-07-09 11:04:43 +0800

    Remove card shadow

--- lib/pembeli/favorit/favorit_page.dart
+++ lib/pembeli/favorit/favorit_page.dart
@@ -212,13 +212,6 @@ class _FavoritPageState extends State<FavoritPage> {
                                       decoration: BoxDecoration(
                                         color: Colors.white,
                                         borderRadius: BorderRadius.circular(16),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.black.withOpacity(0.05),
-                                            blurRadius: 6,
-                                            offset: const Offset(0, 2),
-                                          ),
-                                        ],
                                       ),
                                       child: Row(
                                         crossAxisAlignment: CrossAxisAlignment.center, // ← tengah
