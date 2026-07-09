ad5d404d2ea60878c8aaeefcd77d533f00e40571
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-07-09 17:24:39 +0800

    App's name and icon update

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "pnbfoods");
+    gtk_header_bar_set_title(header_bar, "PNBFoods");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "pnbfoods");
+    gtk_window_set_title(window, "PNBFoods");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
